1ac885d9b19d358c167a76632776f5c64312426e
austinegri <ategri@mac.com>
2024-05-04 17:14:43 -0400

    Remove unnecessary converter lines

--- app/src/main/java/nhlgameupdatelambda/ddb/LinkedHashSetPlayConverter.java
+++ app/src/main/java/nhlgameupdatelambda/ddb/LinkedHashSetPlayConverter.java
@@ -1,11 +1,9 @@
 package nhlgameupdatelambda.ddb;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import nhlgameupdatelambda.data.playbyplay.Play;
 import software.amazon.awssdk.enhanced.dynamodb.AttributeConverter;
 import software.amazon.awssdk.enhanced.dynamodb.AttributeValueType;
-import software.amazon.awssdk.enhanced.dynamodb.DefaultAttributeConverterProvider;
 import software.amazon.awssdk.enhanced.dynamodb.EnhancedType;
 import software.amazon.awssdk.enhanced.dynamodb.internal.converter.attribute.DocumentAttributeConverter;
 import software.amazon.awssdk.enhanced.dynamodb.mapper.ImmutableTableSchema;
@@ -17,15 +15,9 @@ import java.util.stream.Collectors;
 
 @Slf4j
 public final class LinkedHashSetPlayConverter implements AttributeConverter<LinkedHashSet<Play>> {
-    private static final ObjectMapper mapper = new ObjectMapper();
     private static final ImmutableTableSchema<Play> PLAY_TABLE_SCHEMA = ImmutableTableSchema.create(Play.class);
     private static final AttributeConverter<Play> playConverter = DocumentAttributeConverter.create(PLAY_TABLE_SCHEMA,
             EnhancedType.of(Play.class));
-    private final DefaultAttributeConverterProvider defaultAttributeConverterProvider = DefaultAttributeConverterProvider.create();
-    private final AttributeConverter<String> stringAttributeConverter = defaultAttributeConverterProvider
-            .converterFor(EnhancedType.of(String.class));
-    private final AttributeConverter mapAttributeConverter = defaultAttributeConverterProvider
-            .converterFor(EnhancedType.mapOf(String.class, String.class));
 
     @Override
     public AttributeValue transformFrom(LinkedHashSet<Play> input) throws UncheckedIOException{
